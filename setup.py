cff2f1104a86573569500d41e69be54b90b596c6
happyleaves <happyleaves.tfr@gmail.com>
2018-02-27 17:28:38 -0500

    update login

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 setup(
     name='fedexdeliverymanager',
-    version='1.0.5',
+    version='1.0.6',
     description='Python 3 API for Fedex Delivery Manager, a way to track packages.',
     url='https://github.com/happyleavesaoc/python-fedexdeliverymanager/',
     license='MIT',
     author='happyleaves',
     author_email='happyleaves.tfr@gmail.com',
     packages=['fedexdeliverymanager'],
-    install_requires=['beautifulsoup4==4.5.1', 'python-dateutil==2.6.0', 'requests==2.12.4'],
+    install_requires=['beautifulsoup4==4.5.1', 'python-dateutil==2.6.0', 'requests'],
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
